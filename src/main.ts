d7fd0ce24e5180f68e37ac42ccc5edf4efa2f8c8
nam20 <nam741585@naver.com>
2024-04-18 22:04:30 +0900

    feat: Vuetify 테마 적용

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,24 @@
-/**
- * main.ts
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import { router } from './router';
+import vuetify from './plugins/vuetify';
+import '@/scss/style.scss';
+import PerfectScrollbar from 'vue3-perfect-scrollbar';
+import VueApexCharts from 'vue3-apexcharts';
+import VueTablerIcons from 'vue-tabler-icons';
 
-// Plugins
-import { registerPlugins } from '@/plugins'
+import { fakeBackend } from '@/utils/helpers/fake-backend';
 
-// Components
-import App from './App.vue'
+// print
+import print from 'vue3-print-nb';
 
-// Composables
-import { createApp } from 'vue'
-
-const app = createApp(App)
-
-registerPlugins(app)
-
-app.mount('#app')
+const app = createApp(App);
+fakeBackend();
+app.use(router);
+app.use(PerfectScrollbar);
+app.use(createPinia());
+app.use(VueTablerIcons);
+app.use(print);
+app.use(VueApexCharts);
+app.use(vuetify).mount('#app');
